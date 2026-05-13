914586888defbf66554550b2b3319d8c8a0e90da
Sandeep Yadav <sn594064@dal.ca>
2026-05-13 16:17:17 -0300

    finished lab1

---
+++ lab1/logger-lab/src/task1.py
@@ -0,0 +1,3 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.debug('This will get logged')
---
+++ lab1/logger-lab/src/task3a.py
@@ -0,0 +1,3 @@
+import logging
+logger = logging.getLogger('example_logger')
+logger.warning('This is a warning')
